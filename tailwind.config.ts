05f5af1e18529043676179b29530ee418ea5bd25
Robin <robin.django@gmail.com>
2024-01-09 20:10:24 +0100

    Start

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,20 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'purple': '#5115F7',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateColumns: {
+        'desktop': '12.5vw repeat(24, 1fr) 12.5vw',
+      },
+      gap: {
+        'desktop': '0.625vw',
+      },
+      fontFamily: {
+        lato: ['var(--font-lato)'],
       },
     },
   },
