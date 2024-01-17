d7763a5793bc37f10535fe4809ee5dddf2c07f51
Robin <robin.django@gmail.com>
2024-01-17 11:35:21 +0100

    Update Activities.tsx

--- src/components/Activities/Activities.tsx
+++ src/components/Activities/Activities.tsx
@@ -9,11 +9,11 @@ const Activities = async (props: Props) => {
   return (
     <main className="p-5 md:col-start-2 md:col-span-1 md:row-start-1 md:row-span-1">
       <CurrentTemp temperature={tempData} />
-      <section className="flex flex-col @container/activities">
+      <section className="flex flex-col">
         <span className="text-2xl mt-8 block">Some things you could do:</span>
         <ActivitiesList possible={true} temperature={tempData.temp} />
       </section>
-      <section className="flex flex-col @container/activities">
+      <section className="flex flex-col">
         <span className="text-2xl mt-8 block sm:mt-12">
           Some things you should not do:
         </span>
